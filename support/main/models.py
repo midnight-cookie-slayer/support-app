cda2bef0e56c11318eed37b0bb852636a2267835
Sergey <chloegray618@gmail.com>
2021-10-06 15:46:51 +0300

    add Support table

--- support/main/models.py
+++ support/main/models.py
@@ -3,11 +3,20 @@ from django.db import models
 
 class User(models.Model):
 
-    name = models.CharField('Имя', max_length = 75)
-    message = models.TextField('Сообщение', default='Нет сообщения')
-    is_support = models.BooleanField('Сапорт?', default=False)
-    is_done = models.BooleanField('Решено?', default=False)
-    is_frozen = models.BooleanField('Заморожено?', default=False)
+    name = models.CharField('Имя', max_length=75)
+    message = models.TextField('Сообщение')
+    is_done = models.BooleanField('Решено', default=False)
+    is_frozen = models.BooleanField('Заморожено', default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Support(models.Model):
+
+    name = models.CharField('Имя', max_length=75)
+    user_name = models.CharField('Ответил пользователю', max_length=75)
+    message = models.TextField('Ответ')
 
     def __str__(self):
         return self.name
--- support/main/views.py
+++ support/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
+from .models import Support
 
 
 def index(request):
